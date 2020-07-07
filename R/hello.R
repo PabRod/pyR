eceecf028dbd29fb88f3ee7ad5fb48eb91de6fb2
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2020-07-07 15:34:51 +0200

    Don't tell Travis about Python
    Added a very basic reticulated Python script. Let's see if Travis figures out what's going on

--- R/hello.R
+++ R/hello.R
@@ -1,25 +1,9 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-#' Hello
+#' Check Python core
 #'
 #' @return
 #' @export
 #'
-#' @examples
-hello <- function() {
-  print("Hello, world!")
+python_core <- function() {
+  reticulate::py_run_string("text = 'Python core working correctly'")
+  print(reticulate::py$text)
 }
-
